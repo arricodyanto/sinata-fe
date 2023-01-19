5884d12a8ae30e2387d76e4768124a7390f15bb3
arricodyanto <arricohandyanto@gmail.com>
2023-01-20 00:49:20 +0700

    fix vh

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -12,16 +12,16 @@ export default function SignIn() {
         <Head>
             <title>Sign in - Sinata</title>
         </Head>
-        <Box className='grid place-items-center mt-12 h-[100vh] bg-light'>
+        <Box className='grid place-items-center bg-light' sx={{height: {xs: '100vh'}}}>
             <Container sx={{ width: {
                 xs: 360,
                 sm: 760,
                 lg: 1100,
                 },
             }}>
-                <Box my={9} gridColumn={2} className="border rounded-md bg-white shadow-sm -mb-[14rem]">
-                    <Grid container py={4}>
-                        <Grid item lg={6} sm={6} px={4}>
+                <Box my={9} gridColumn={2} className="border rounded-md bg-white shadow-sm">
+                    <Grid container>
+                        <Grid item lg={6} sm={6} p={4}>
                             <HeaderBox header='Selamat Datang!' subheader='Silakan melakukan proses login untuk masuk ke dashboard Anda.' />
                             <SignInForm />
                             <Stack>
@@ -31,13 +31,13 @@ export default function SignIn() {
                                 <GoogleButton />
                             </Box> */}
                         </Grid>
-                        <Grid item lg={6} sm={6} sx={{ display: { xs: 'none', sm: 'block', lg: 'block' },}} className='-my-8 relative'>
+                        <Grid item lg={6} sm={6} sx={{ display: { xs: 'none', sm: 'block', lg: 'block' },}} className='relative'>
                             <CoverSide src='/images/auth-bg.jpg' sentence='&quot;Everyday is a Good Day!&quot;' subject='Public Relation UNS' />
                         </Grid>
                     </Grid>
                 </Box>
             </Container>
-            <Box className='grid place-items-center'>
+            <Box sx={{marginTop: {xs: '-7rem', sm: '-28rem', lg: '-10rem'}}}>
                 <Typography variant='body2' className='text-gray-500'>Belum memiliki akun? <Link href='/sign-up' className='text-sky-700 underline'>Sign Up</Link></Typography>
             </Box>
         </Box>
