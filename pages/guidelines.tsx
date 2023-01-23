f30c1f18e973e3db1f89d4499cdcac4e09bc7aef
arricodyanto <arricohandyanto@gmail.com>
2023-01-24 00:55:28 +0700

    add events page, guidelines page

---
+++ pages/guidelines.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import HeaderPages from '../components/molecules/HeaderPages'
+
+export default function GuideLines() {
+  return (
+    <Box className='bg-white'>
+        <HeaderPages titlePage='Panduan - Sistem Informasi Manajemen Pelayanan dan Berita' srcImg='/images/bg-guides.jpg' />
+    </Box>
+  )
+}
