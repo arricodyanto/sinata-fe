86a74af1e569bbf0d58bf1ae877f48b830f8fd71
arricodyanto <arricohandyanto@gmail.com>
2023-01-26 23:29:28 +0700

    restyling event card

--- components/organism/AllEvents/index.tsx
+++ components/organism/AllEvents/index.tsx
@@ -1,19 +1,7 @@
-import {
-  Card,
-  CardActions,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  Divider,
-  Grid,
-  IconButton,
-  Typography,
-} from "@mui/material";
+import { Avatar, Box, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Divider, Grid, IconButton, Typography } from "@mui/material";
 import React from "react";
-import EventCard from "../../molecules/EventCard";
 import event from "../../../json/events.json";
 import Link from "next/link";
-import VisibilityIcon from "@mui/icons-material/Visibility";
 import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
 import ScheduleOutlinedIcon from "@mui/icons-material/ScheduleOutlined";
 
@@ -23,70 +11,55 @@ export default function AllEvents() {
       <Grid container spacing={3}>
         {event.map((item) => {
           return (
-            <Grid
-              key={item.id}
-              item
-              xs={12}
-              sm={4}
-              className="justify-center flex"
-            >
-              <Card
-                sx={{ width: 330 }}
-                variant="outlined"
-                className="rounded-xl shadow-lg border"
-              >
-                <CardHeader
-                  className="text-center py-3 h-7"
-                  title={
-                    <Typography
-                      variant="caption"
-                      color="text.primary"
-                      className="truncate"
-                    >
-                      <CalendarMonthOutlinedIcon sx={{ fontSize: 10 }} />{" "}
-                      {item.date}
-                      <ScheduleOutlinedIcon
-                        sx={{ fontSize: 10, marginLeft: 1 }}
-                      />{" "}
-                      {item.date}
-                    </Typography>
+            <Grid key={item.id} item xs={12} sm={4} className="justify-center flex" >
+              <Card sx={{ width: 330 }} variant="elevation" className="rounded-xl shadow-lg border relative" >
+                {/* <CardHeader className="text-center py-3 h-7" title={ 
+                  <Typography variant="caption" color="text.primary" className="truncate">
+                    <CalendarMonthOutlinedIcon sx={{ fontSize: 10 }} />{" "}
+                    {item.date}
+                    <ScheduleOutlinedIcon sx={{ fontSize: 10, marginLeft: 1 }} />{" "}
+                    {item.time}
+                  </Typography>
                   }
-                />
-                <CardMedia
-                  component="img"
-                  image={item.image}
-                  alt="Event Image"
-                />
-                <CardContent sx={{ height: 120 }}>
-                  <Typography
-                    variant="body1"
-                    color="text.primary"
-                    className="mb-1 truncate"
-                  >
+                /> */}
+                <Box className="relative">
+                  <CardMedia component="img" image={item.image} alt="Event Image" />
+                  <Box className="bg-white py-0 px-1 absolute top-0 left-0 m-3 rounded-lg">
+                    <Typography variant="caption" className="text-gray-600">
+                      {item.sifat}
+                    </Typography>
+                  </Box>
+                  <Box className="bg-white py-1 px-3 absolute bottom-0 right-0 m-3 rounded-md">
+                    <Typography variant="body2" className="text-gray-500 flex">
+                      <Avatar alt={item.publisher} src={item.avatar} sx={{ width: 20, height: 20 }} className='mr-1'/>
+                      {item.publisher}
+                    </Typography>
+                  </Box>
+                </Box>
+                <CardContent className="max-h-44">
+                  <Typography variant="body1" color="text.primary" className="mb-1 truncate" >
                     {item.title}
                   </Typography>
-                  <Typography
-                    variant="body2"
-                    color="text.secondary"
-                    className="leading-[1rem] line-clamp-3"
-                  >
+                  <Typography variant="body2" color="text.secondary" className="leading-[1rem] line-clamp-3" >
                     {item.description}
                   </Typography>
+                  <Divider className="my-4"/>
+                  <Grid container spacing={1} className="">
+                    <Grid item xs={4} className="flex justify-center">
+                      Lorem.
+                    </Grid>
+                  </Grid>
                 </CardContent>
-                <Divider />
-                <CardActions className="justify-center h-7">
-                  <Link href="/">
-                    <IconButton aria-label="more" color="primary">
-                      <VisibilityIcon fontSize="small" />
-                      <Typography variant="caption" className="pl-1">
-                        More
-                      </Typography>
-                    </IconButton>
+                <CardActions className="px-3">
+                  <Link href="#">
+                    <Button variant="text" color="primary" size="small">
+                      Lihat Detail
+                    </Button>
                   </Link>
                 </CardActions>
               </Card>
             </Grid>
-          );
+          )
         })}
       </Grid>
     </>
